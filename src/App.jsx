b973221f559580876d6532029af961b20d466102
Pharoah_devil <97384627+RishiKumar156@users.noreply.github.com>
2023-12-03 18:29:45 +0530

    dialog success load

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
+import { useState } from "react";
 import DialogBox from "./Components/DialogBox";
 import Home from "./Components/Home/Home";
 import Navigation from "./Components/Nav/Navigation";
 
 export default function App() {
+  const [Open, setOpen] = useState(false);
   return (
     <>
-      <Home />
+      <button onClick={() => setOpen(true)}> Open dialog</button>
+      <DialogBox
+        isOpen={Open}
+        onClose={() => setOpen(false)}
+        data={"Hello"}
+      ></DialogBox>
     </>
   );
 }
--- src/Components/DialogBox.jsx
+++ src/Components/DialogBox.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 
-export default function DialogBox({ isOpened, closed, data }) {
-  return isOpened ? (
-    <div>
-      DialogBox
-      <button onClick={closed}> close </button>
+export default function DialogBox({ isOpen, onClose, data }) {
+  if (!isOpen) return null;
+  return (
+    <div
+      style={{ backgroundColor: "rgba(0,0,0,0.5)" }}
+      className="w-full h-screen fixed top-0 left-0 flex items-center justify-center"
+    >
+      <div className="flex w-[80%] h-[70%] bg-white rounded-sm ">
+        <button onClick={onClose}>Close</button>
+      </div>
     </div>
-  ) : null;
+  );
 }
