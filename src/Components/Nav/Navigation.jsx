6e070c6e8489e85a7a7ed0ca9dcdc965a720f863
Pharoah_devil <97384627+RishiKumar156@users.noreply.github.com>
2023-12-06 20:03:16 +0530

    DB connection work's perfect

--- src/Components/Nav/Navigation.jsx
+++ src/Components/Nav/Navigation.jsx
@@ -1,7 +1,23 @@
+import { useState } from "react";
+import Profile from "../Profile/Profile";
+
 export default function Navigation() {
+  const [isOpen, setOff] = useState(false);
   return (
-    <div className="fixed top-0 left-0 w-full h-[13vh] p-5 bg-[#FF8C8C]">
-      Navigation
+    <div className="fixed flex items-center justify-between top-0 left-0 w-full h-[13vh] p-5 bg-[#FF8C8C]">
+      <h1>Navigation</h1>
+      {isOpen ? (
+        <Profile />
+      ) : (
+        <div className="mr-[4rem]">
+          <button className="text-[#FF8C8C] transition duration-300 hover:bg-[#FF8C8C] hover:text-white font-semibold text-sm py-2 px-6 bg-[white] mx-[0.5rem] rounded-sm">
+            Login
+          </button>
+          <button className="text-[#FF8C8C] transition duration-300 hover:bg-[#FF8C8C] hover:text-white font-semibold text-sm py-2 px-6 bg-[white] mx-[0.5rem] rounded-sm">
+            SignUp
+          </button>
+        </div>
+      )}
     </div>
   );
 }
---
+++ src/Components/Profile/Profile.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Profile({ data, api }) {
+  return <div>Profile</div>;
+}
