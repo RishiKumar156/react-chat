e546570f693bf14714949f3a0486efc46a3afed7
Pharoah_devil <97384627+RishiKumar156@users.noreply.github.com>
2023-12-09 20:49:52 +0530

    cart functionality

--- cart/views.py
+++ cart/views.py
@@ -3,32 +3,25 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from .serializer import CartSerializer
-from backend.views import test_token  # Assuming you have a utility function for token validation
 
 @api_view(['POST'])
 def CreateCart(request):
-    # Ensure we have a valid HttpRequest object
-    if not isinstance(request, HttpRequest):
-        return Response({'message': 'Invalid request'}, status=status.HTTP_400_BAD_REQUEST)
-
-    # Read the Authorization header
-    authorization_header = request.headers.get('Authorization', '')
-
-    # Check if authorization_header is not empty and starts with 'Bearer'
-    if not authorization_header.startswith('Token '):
-        return Response({'message': 'Invalid Authorization header format'}, status=status.HTTP_401_UNAUTHORIZED)
-
-    # Extract the token from the header
-    token = authorization_header.split(' ')[1]
-
-    # Validate the token using your utility function
-    isAuthenticated = test_token(request)
-
+    token = request.headers.get('Authorization')
+    response = Authorize(token)
+    print(response)
     serializer = CartSerializer(data=request.data)
-
     if serializer.is_valid():
-        if isAuthenticated:
-            serializer.save()
-            return Response({'success': 'CartCreated'}, status=status.HTTP_201_CREATED)
+        serializer.save()
+        return Response({'success': 'CartCreated'}, status=status.HTTP_201_CREATED)
 
     return Response({'message': 'Cart Has Not been Created'}, status=status.HTTP_400_BAD_REQUEST)
+
+
+from rest_framework.decorators import authentication_classes, permission_classes
+from rest_framework.authentication import SessionAuthentication , TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+@api_view(['GET'])
+@authentication_classes([SessionAuthentication, TokenAuthentication])
+@permission_classes([IsAuthenticated])
+def Authorize(request):
+    return Response("Passed for {}".format(request.user.email))
