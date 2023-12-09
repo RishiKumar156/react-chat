31a5465834c1fc204a79a58f6489906f5cfe68d0
Pharoah_devil <97384627+RishiKumar156@users.noreply.github.com>
2023-12-09 16:53:20 +0530

    cart backend development complete

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
+from .serializer import CartSerializer
+from backend.views import test_token
 # Create your views here.
+
+
+@api_view(['POST'])
+def CreateCart(request):
+    serializer = CartSerializer(request.data)
+    isAuthenticated = test_token()
+    if serializer.is_valid():
+        serializer.save()
+        return Response({'success': 'CartCreated'}, status=status.HTTP_201_CREATED)
+    return Response({'message' : 'Cart Has Not been Created'}, status=status.HTTP_400_BAD_REQUEST)
